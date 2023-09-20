7bef51e12971ef7ba7f89558df9639400c3288e3
Suprun Oleksandr <1specktr1@gmail.com>
2023-09-20 17:39:06 +0300

    add HW7

--- src/main/coffee_order/CoffeeOrderBoard.java
+++ src/main/coffee_order/CoffeeOrderBoard.java
@@ -1,3 +1,5 @@
+package main.coffee_order;
+
 import java.util.*;
 
 public class CoffeeOrderBoard {
--- src/main/coffee_order/Main.java
+++ src/main/coffee_order/Main.java
@@ -1,3 +1,5 @@
+package main.coffee_order;
+
 public class Main {
     public static void main(String[] args) {
 
